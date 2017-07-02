226e9fdb52b6618c09ce94889d4fb9ff43953b3b
MichaelRechenberg <mikerechenberg@owc.net>
2017-07-02 18:12:51 -0500

    Started Chapter 4 and got up to but not including 4.3. Wrote a silly function called playerReview to get a concrete example of pattern matching. Should write a function using guards next. Added more notes

--- hello-world/app/Main.hs
+++ hello-world/app/Main.hs
@@ -5,5 +5,17 @@ import Lib
 main :: IO ()
 main = do
   print(zip [1..100] (fizzBuzz [1..100]))
+  -- Should print "He's a superstar"
+  print(playerReview ("Mike", "Cubs", 20) )
+  -- Should print "Cody is garbage"
+  print(playerReview ("Cody", "Packers", 20) )
+  -- Should print "Cody is a bit young, but perhaps Bears needs
+  --    new blood"
+  print(playerReview ("Cody", "Bears", 20) )
+  -- Should print "Can't say anything definitively"
+  print(playerReview ("Uncle Bob", "Bears", 55) )
+  -- Should print "I didn't know he played"
+  print(playerReview ("Uncle Bob", "Cubs", 55) )
+  print(playerReview ("ASDF", "Invalid", (-1)) )
 
 
--- hello-world/src/Lib.hs
+++ hello-world/src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc, fizzBuzz, doubleUs
+    ( someFunc, fizzBuzz, doubleUs, playerReview
     ) where
 
 import Data.Char
@@ -19,6 +19,17 @@ fizzBuzz xs = [if (x `mod` 3 == 0 && x `mod` 5 == 0) then "FizzBuzz"
               else if x `mod` 15 == 0 then "Buzz"
               else show x 
               | x <- xs]
+
+--Silly function that returns a "review" of the player
+-- (Player name, team, age)
+--Meant to concretize patterns
+playerReview :: (String, String, Int) -> String
+playerReview ("Mike", _, _) = "He's a superstar"
+playerReview (name, "Packers", _) =  name ++ " is garbage"
+playerReview (name, team, 20) = name ++ " is bit young, but perhaps " 
+                                ++ team ++ " needs some new blood"
+playerReview ("Uncle Bob", "Cubs", _) = "I didn't know he played"
+playerReview x = "Can't say anything definitively"
 
 
 
