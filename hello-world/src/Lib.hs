5bb6baa240072a5764aa1c452d3419777e730814
MichaelRechenberg <mikerechenberg@owc.net>
2017-07-02 16:14:50 -0500

    Up through Chapter 2 of Learn you a Haskell. Wrote a somewhat ugly FizzBuzz for practice. Kinda glossed over the tuple part but the zip function is nice. Hopefully I get into a more detailed discussion on definitions of functions..the whole :: part of the declaration is still fuzzy to me

--- hello-world/src/Lib.hs
+++ hello-world/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( someFunc, fizzBuzz, doubleUs
     ) where
+
+import Data.Char
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+doubleUs :: Int -> Int -> Int
+doubleUs x y = 2*x + x*y
+
+-- Bad first attempt at FizzBuzz in Haskell
+fizzBuzz xs = [if (x `mod` 3 == 0 && x `mod` 5 == 0) then "FizzBuzz" 
+              else if x `mod` 3 == 0 then "Fizz"
+              else if x `mod` 15 == 0 then "Buzz"
+              else "Invalid"
+              | x <- xs]
+
+
+
